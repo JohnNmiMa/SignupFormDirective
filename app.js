e5a0e18c9a2d5d46daa8ddf29e4c679c88adb077
John <johnmarksjr@gmail.com>
2014-09-10 20:59:24 -0600

    Implementation complete

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = angular.module('signupApp', [])
+
+.controller('SignupCtrl', function($scope) {
+    $scope.data = {};
+    $scope.init = function() {
+        $scope.data.firstName = "";
+        $scope.data.lastName = "";
+        $('.firstName').focus();
+    }
+    $scope.submit = function() {
+        if($scope.signupForm.$valid) {
+            console.log('Valid Form Submitted: ', $scope.data);
+        } else {
+            $scope.submitted = false;
+            console.log('The Form "myForm" is invalid');
+        }
+    }
+    $scope.init();
+})
+
+.directive('optIn', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'app-template.html',
+        replace: false
+    }
+});
